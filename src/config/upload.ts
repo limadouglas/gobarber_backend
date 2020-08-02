91bd23800a30388d512375d586c67db7152fb89d
Douglas Henrique <dhlima.essence@rd.com.br>
2020-08-02 14:54:04 -0300

    nivel 4 - Personalizando para produção

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,18 +1,36 @@
-import multer from 'multer';
+import multer, { StorageEngine } from 'multer';
 import path from 'path';
 import crypto from 'crypto';
+
+interface IUploadConfig {
+  driver: 'disk' | 's3';
+  tmpFolder: string;
+  uploadsFolder: string;
+  multer: { storage: StorageEngine };
+  config: { disk: {}; aws: { bucket: string } };
+}
 
 export const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
 export default {
+  driver: process.env.STORAGE_DRIVER,
+
   tmpFolder,
   uploadsFolder: path.resolve(tmpFolder, 'uploads'),
-  storage: multer.diskStorage({
-    destination: tmpFolder,
-    filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('HEX');
-      const fileName = `${fileHash}-${file.originalname}`;
-      callback(null, fileName);
-    },
-  }),
-};
+
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpFolder,
+      filename(request, file, callback) {
+        const fileHash = crypto.randomBytes(10).toString('HEX');
+        const fileName = `${fileHash}-${file.originalname}`;
+        callback(null, fileName);
+      },
+    }),
+  },
+
+  config: {
+    disk: {},
+    aws: { bucket: 'app-gobarber-service' },
+  },
+} as IUploadConfig;
